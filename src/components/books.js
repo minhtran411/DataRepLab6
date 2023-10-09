9b98cf8f3750760f6e9e279a176d5c57d3d4baa7
Minh Tran <g00365411@atu.ie>
2023-10-09 14:45:51 +0000

    Update books

--- src/components/books.js
+++ src/components/books.js
@@ -5,10 +5,11 @@ function Books(props) {
     //Everything between {} in react is javascript
     //mybooks is an array => apply map function onto it
     //a call back function
+    //we give them a key as an identifier
     return props.myBooks.map(
         
     (book)=>{
-        return <BookItem myBook = {book}></BookItem>
+        return <BookItem myBook = {book} key={book.isbn}></BookItem>
     }
 
     );
